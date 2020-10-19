73e7ebaaabcc3d9e0119d6829fefc80eee71c035
vidahoro <vdome@unicauca.edu.co>
2020-10-19 12:12:26 -0500

    1er cambio desde netbeans

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -17,6 +17,8 @@ public class Test {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("jijijkkkki");
+        System.out.println("sasdasd");
+                
                 
     }
     
