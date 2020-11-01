e2ac39f20f103d70365a00a6f9f7e934ed0fd28a
Juan D Barrera <jdbarrera@unicauca.edu.co>
2020-10-31 19:04:01 -0500

    Creación Clases

---
+++ src/Modelo/Restaurante.java
@@ -0,0 +1,77 @@
+package Modelo;
+
+import java.util.*;
+
+public class Restaurante {
+
+    private ArrayList<Administrador> Admins;
+    private ArrayList<Usuario> Cliente;
+    private ArrayList<Comida> Comidas;
+    private ArrayList<Factura> RegistroFacturas;
+
+    public void IngresoAdmin() {
+        
+    }
+    public void IngresoCliente() {
+        
+    }
+    public void ModificarAdmin() {
+        
+    }
+    public void RegistrarComida() {
+        
+    }
+    public void ModificarComida() {
+        
+    }
+    public void EliminarComida() {
+        
+    }
+    public void ListarComidas() {
+        
+    }
+    public void ListarTiposMasSolicitados() {
+        
+    }
+
+    public void ListarComidasSolicitadasSemana() {
+        
+    }
+
+    public void IngresarCliente() {
+        
+    }
+
+    public void RegistrarCliente() {
+        
+    }
+
+    public void VerMenu() {
+        
+    }
+
+    public void RealizarPedido() {
+        
+    }
+
+    public void NotificarPedidoRealizado() {
+        
+    }
+
+    public void NotificarPedidoOrdenado() {
+        
+    }
+
+    public void GenerarFactura() {
+        
+    }
+
+    public void PrecioTotal() {
+        
+    }
+
+    public void CantidadComida() {
+        
+    }
+
+}
